b68b05167aa72d6fc07e175fa20a8bdb6aea2fa5
Amund <72158192+Laudster@users.noreply.github.com>
2024-09-03 23:05:56 +0200

    Rushet til bruker lagde rom

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from urllib.parse import parse_qs
 import json
+import os
 
 app = Flask(__name__)
 
-FILENAME = "messages.json"
+FILEPATH = "saves/Global.json"
 
 def update_file(file, data):
     file.seek(0)
@@ -15,32 +17,77 @@ def globall():
     messages : str
     display_name = request.args.get("display_name")\
 
-    with open(FILENAME, "r") as file:
+    with open(FILEPATH, "r") as file:
         data = json.load(file)
         messages = data["messages"]
 
     return render_template("global.html", messages=messages, displayer=display_name)
+
+@app.route("/<room>")
+def room(room):
+    if os.path.exists(f"saves/{room}.json"):
+        messages : str
+        display_name = request.args.get("display_name")
+
+        with open(f"saves/{room}.json", "r") as file:
+            data = json.load(file)
+            messages = data["messages"]
+
+        return render_template("global.html", messages=messages, displayer=display_name)
+    
+    return redirect(url_for("globall", display_name=request.args.get("display_name")))
 
 @app.route("/new-message", methods=["POST"])
 def post():
     message = request.form.get("message")
     display_name = request.form.get("display_name")
 
-    with open(FILENAME, "r+") as file:
+    room = request.form.get("room")
+    room = room[1:len(room)]
+
+    if room == "" or room == "/":
+        room = "Global"
+
+    with open(f"saves/{room}.json", "r+") as file:
         message_data = json.load(file)
         if not display_name:
             display_name = "User"
         message_data["messages"].append({"value": message, "displayname": display_name+": "})
         update_file(file, message_data)
 
-    return redirect(url_for("globall", display_name=display_name))
+    return jsonify()
 
 @app.route("/get-messages", methods=["GET"])
 def get_messages():
-    with open(FILENAME, "r") as file:
+    room = request.args.get("room")
+
+    room = room[1:len(room)]
+
+    if room == "/" or room == "":
+        room = "global"
+    elif os.path.exists(f"saves/{room}.json") == True:
+        messages : str
+
+        with open(f"saves/{room}.json", "r") as file:
+            message_data = json.load(file)
+
+        return jsonify(message_data)
+
+    with open(f"saves/{room}.json", "r") as file:
         message_data = json.load(file)
 
     return jsonify(message_data)
+
+@app.route("/new-room", methods=["POST"])
+def new_room():
+    room_name = request.form.get("room_name")
+
+    if not os.path.exists("saves/" + room_name + ".json"):
+        with open("saves/" + room_name + ".json", "w") as file:
+            data = {"messages": []}
+            json.dump(data, file, indent=4)
+    
+    return jsonify()
 
 
 if __name__ == "__main__":
